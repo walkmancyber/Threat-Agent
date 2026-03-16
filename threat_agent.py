98380f6b046c0312283635031d3bebe36254b9ec
Alejandro Imbert <imbert.16@gmail.com>
2026-03-16 12:22:50 -0600

    feat: threat_agent

---
+++ threat_agent.py
@@ -0,0 +1,26 @@
+import sys
+
+from core.investigation_engine import run_investigation
+from utils.validators import validate_target
+from utils.reporter import print_report
+
+
+def main():
+
+    if len(sys.argv) < 2:
+        print("Usage: python threat_agent.py <IP|domain|URL>")
+        sys.exit(1)
+
+    target = sys.argv[1]
+
+    if not validate_target(target):
+        print("Invalid target format")
+        sys.exit(1)
+
+    result = run_investigation(target)
+
+    print_report(result)
+
+
+if __name__ == "__main__":
+    main()
